18fbaa6335eb9bd5cfac5ee169b328848a16a7b6
Nitesh Paudel <niteshpaudel96@gmail.com>
2024-12-06 17:41:55 +0000

    Writing up JavaScript for the audio stream part.Made a new route that handles LLM work of transcriber.Base Backend to frontend connection is complete. First making a <Record and transcribe> system. Then i will probably merge to master a Real time transcriber branch will probably use small to medium speech recognition LLM using hugging face transformers.

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    transcription = "Hello World"
+    return jsonify({'transcription': transcription})
+
+
 if __name__ == "__main__":
     app.run(debug=True)
