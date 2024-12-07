8584b4b32b10c222ab627bec11af3e1a1acec8b0
Nitesh Paudel <niteshpaudel96@gmail.com>
2024-12-07 16:19:37 +0000

    using audio array to resolve the streaming problem.

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, jsonify, render_template
 import torch
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 import numpy as np
-from pydub import AudioSegment
+import audioread
+import librosa
+import os
 
 app = Flask(__name__)
 
@@ -11,7 +13,7 @@ model_name = "jonatasgrosman/wav2vec2-large-xlsr-53-english"
 processor = Wav2Vec2Processor.from_pretrained(model_name) 
 model = Wav2Vec2ForCTC.from_pretrained(model_name)
 
-
+#make a home page pointer on '/' as index.html
 @app.route('/', methods=['POST','GET'])
 def index():
     return render_template('index.html')
@@ -21,22 +23,46 @@ def index():
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
     audio_file = request.files['audio'] 
-    # Read the audio file using pydub 
-    audio = AudioSegment.from_file(audio_file) 
-    #reshample the audio to 16000hz
-    audio = audio.set_frame_rate(16000)
-    #convert the audio to a numpy array
-    audio_data = np.array(audio.get_array_of_samples()) 
-    
-    if audio.channels > 1:
-        audio_data = audio_data.reshape((-1, audio.channels)).mean(axis=1)
-    
-    audio_data = audio_data / np.iinfo(audio_data.dtype).max
 
-    input_values = processor(audio_data, return_tensors="pt", sampling_rate=16000).input_values 
-    logits = model(input_values).logits 
-    predicted_ids = torch.argmax(logits, dim=-1) 
-    transcription = processor.decode(predicted_ids[0]) 
+    #define a permanent file path
+    audio_path = "uploaded_audio_chunk.wav"
+
+    #save the audio file
+    audio_file.save(audio_path)
+
+     # Read the audio file using soundfile
+    try: 
+        with audioread.audio_open(audio_path) as f: 
+            sr = f.samplerate 
+            channels = f.channels 
+            
+            audio_array = [] 
+            for buf in f: 
+                audio_array.extend(np.frombuffer(buf, dtype=np.int16)) 
+                
+        audio_array = np.array(audio_array, dtype=np.float32)
+
+        #if audio is two channel change it to 1 channel
+        if channels >1:
+            audio_array = audio_array.reshape((-1,channels)).mean(axis=1)
+
+        #if audio is not 16000hz change it to 16khz
+        if sr !=16000:
+            audio_array = librosa.resample(audio_array, sr, 16000)
+            sr = 16000
+
+        input_values = processor(audio_array, return_tensors="pt", sampling_rate=16000).input_values 
+        logits = model(input_values).logits 
+        predicted_ids = torch.argmax(logits, dim=-1) 
+        transcription = processor.decode(predicted_ids[0]) 
+    except Exception as e:
+        print(f"Error reading audio file: {e}")
+        return jsonify({'error' : 'Failed to read audio file'})
+    finally:
+        # De;ete the temporary audio file
+        if os.path.exists(audio_path):
+            os.remove(audio_path)
+
     return jsonify({'transcription': transcription})
 
 if __name__ == "__main__":
